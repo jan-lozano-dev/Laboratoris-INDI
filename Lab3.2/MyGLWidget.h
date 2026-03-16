b98be26af43ca73a656b872c9e26a83543ced5e3
JOAN LOZANO FUNOSAS <e8184607@PC134585.epsevg.upc.es>
2026-03-16 12:19:42 +0100

    Lab3.2 [INCOMPLETE] - Matriu transformació 4x4 TG

---
+++ Lab3.2/MyGLWidget.h
@@ -0,0 +1,45 @@
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLWidget>
+#include <QOpenGLShader>
+#include <QOpenGLShaderProgram>
+#include "glm/glm.hpp"
+#include <QKeyEvent> //necesari per rebre el teclat
+#include "glm/gtc/matrix_transform.hpp"
+
+class MyGLWidget : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core
+{
+  Q_OBJECT
+
+  public:
+    MyGLWidget (QWidget *parent=0);
+    ~MyGLWidget ();
+    
+  protected:
+    // initializeGL - Aqui incluim les inicialitzacions del contexte grafic.
+    virtual void initializeGL ();
+
+    // paintGL - Mètode cridat cada cop que cal refrescar la finestra.
+    // Tot el que es dibuixa es dibuixa aqui.
+    virtual void paintGL ();
+ 
+    // resize - Es cridat quan canvia la mida del widget
+    virtual void resizeGL (int width, int height);
+    void modelTransform();
+
+  private:
+    GLuint transLoc; // declarem localització de escalat (en el vertex shader)
+    float scl; // scale
+    virtual void keyPressEvent(QKeyEvent *e); //Volem interactuar amb qt amb el teclat per escalar el triangle.
+
+    void creaBuffers ();
+    void carregaShaders ();
+    
+    // attribute locations
+    GLuint vertexLoc, colorLoc;
+    
+    // Program
+    QOpenGLShaderProgram *program;
+
+    GLuint VAO1;
+    GLint ample, alt;
+};
